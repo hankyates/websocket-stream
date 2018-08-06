966fd4e6b7210b9c1a5ecfb1c54abbd8aeb0f4c0
Hank Yates <hankyates@gmail.com>
2018-08-05 21:25:58 -0500

    progress commit

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+let {
+  range,
+  get,
+  pipe,
+  fromPairs
+} = _
+let {
+  distinctUntilChanged,
+  withLatestFrom,
+  scan,
+  skip,
+  bufferCount,
+  first,
+  concatMap,
+  tap,
+  mergeMap,
+  map,
+  filter,
+  take
+} = rxjs.operators
+
 let url = 'wss://ws-feed.pro.coinbase.com'
-//let url = 'wss://ws.cex.io/ws/'
-//let url = 'wss://ws.coinapi.io/v1/'
-//let url = 'wss://echo.websocket.org'
 
 ws$ = rxjs.webSocket.webSocket(url)
-let data = {
+ws$.next({
   "type": "subscribe",
   "product_ids": [
     "ETH-USD",
@@ -18,24 +36,69 @@ let data = {
       ]
     }
   ]
-}
+})
 
-let heartbeat$ = ws$.pipe(
-  rxjs.operators.filter(v => v.type === 'heartbeat')
+let createStream = (field) => ws$.pipe(
+  rxjs.operators.filter(v => v.type === field),
 )
 
-let ticker$ = ws$.pipe(
-  rxjs.operators.filter(v => v.type === 'ticker')
+let selectPrice = pipe(get('price'), parseFloat)
+let selectSequence = get('sequence')
+
+let ticker$ = createStream('ticker').pipe(map(selectPrice))
+let heartbeat$ = createStream('heartbeat').pipe(map(selectSequence))
+let xAxis = rxjs.interval()
+const BUFFER_SIZE = 10
+let last = n => a => a.slice(Math.max(0, a.length - n - 1), a.length)
+let latestBuffer = last(BUFFER_SIZE)
+let createFx = map => x => map[x]
+let createTrapezoidal = n => f => f.reduce((acc, val, i, f) => {
+  if (i === f.length - 1) return acc
+  let fx = val[1]
+  let fxOne = f[i + 1][1]
+  let xk = val[0]
+  let xkOne = f[i + 1][0]
+  return acc + (
+      (fx + fxOne / 2
+    ) * (
+      xkOne - xk
+    )
+  )
+}, 0)
+let trapezoidal = createTrapezoidal(BUFFER_SIZE)
+
+let fOfX$ = ticker$.pipe(
+  withLatestFrom(xAxis),
+  scan((a, v) => [...latestBuffer(a), v.reverse()], []),
+  map(trapezoidal)
 )
 
-ws$.next(data)
-
-heartbeat$.subscribe(
-  console.log,
+var ctx = document.getElementById('myChart')
+var data = []
+var labels = []
+fOfX$.subscribe(
+  v => {
+    console.log(v)
+    data = _.concat(data, v)
+    labels = _.concat(labels, moment().format('llll'))
+    new Chartist.Line(ctx, {
+      labels: [labels],
+      series: [data]
+    }, {
+      low: 0,
+      showArea: true
+    })
+  },
   console.error,
 )
 
-ticker$.subscribe(
-  console.log,
-  console.error,
-)
+
+// https://www.geeksforgeeks.org/trapezoidal-rule-for-approximate-value-of-definite-integral/
+//def trapezoidal (a, b, n):
+  //h = (b - a) / n
+  //s = (y(a) + y(b))
+  //i = 1
+  //while i < n:
+    //s += 2 * y(a + i * h)
+    //i += 1
+  //return ((h / 2) * s)
