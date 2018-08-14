9bb9b62cfac6a4d4cc7d9f6255e73b1896b9f429
Hank Yates <hankyates@gmail.com>
2018-08-14 18:33:50 -0500

    checking in notes

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ let {
 
 let url = 'wss://ws-feed.pro.coinbase.com'
 
-ws$ = rxjs.webSocket.webSocket(url)
-ws$.next({
+y$ = rxjs.webSocket.webSocket(url)
+y$.next({
   "type": "subscribe",
   "product_ids": [
     "ETH-USD",
@@ -31,19 +31,12 @@ ws$.next({
   ]
 })
 
-const BUFFER_SIZE = 10
 let x$ = rxjs.interval().pipe(
   map(Date.now),
 )
 let createWindow = seconds => ([price, timestamp]) =>  (Date.now() - (seconds * 1000)) < timestamp
-let filterByTenSeconds = _.filter(createWindow(10))
-let filterByHalfMinute = _.filter(createWindow(30))
-let filterByMinute = _.filter(createWindow(60))
-let filterByFiveMinutes = _.filter(createWindow(60 * 5))
-let filterByTenMinutes = _.filter(createWindow(60 * 10))
 let selectPrice = pipe(get('price'), parseFloat)
 
-// buffer: number => number
 let trapezoidal = arr => arr.reduce((acc, val, i) => {
   if (i === arr.length - 1) return acc
   let fx = arr[i][0]
@@ -53,21 +46,24 @@ let trapezoidal = arr => arr.reduce((acc, val, i) => {
   return ((fx + fxNext) / 2) * (xNext - x)
 }, 0)
 
-let createStreamWindow = windowFilter => ws$.pipe(
+let noWindow$ = y$.pipe(
   filter(v => v.type === 'ticker'),
   map(selectPrice),
   withLatestFrom(x$),
-  scan((acc, val) => [
-    ...windowFilter(acc),
-    val
-  ], []),
 )
 
-let tenSec$ = createStreamWindow(filterByTenSeconds)
-let halfMin$ = createStreamWindow(filterByHalfMinute)
-let min$ = createStreamWindow(filterByMinute)
-let fiveMin$ = createStreamWindow(filterByFiveMinutes)
-let tenMin$ = createStreamWindow(filterByTenMinutes)
+let createWindowStream = seconds => noWindow$.pipe(
+  tap(console.log),
+  scan((acc, val) => acc.pipe(filter(createWindow(seconds))), rxjs.empty()),
+  tap(console.log),
+  map(trapezoidal),
+)
+
+let tenSec$ = createWindowStream(10)
+let halfMin$ = createWindowStream(10)
+let min$ = createWindowStream(60)
+let fiveMin$ = createWindowStream(60 * 5)
+let tenMin$ = createWindowStream(60 * 10)
 
 let integral$ = rxjs.zip(
   tenSec$,
@@ -75,10 +71,6 @@ let integral$ = rxjs.zip(
   min$,
   fiveMin$,
   tenMin$,
-).pipe(
-  map(
-    arr => [arr.map(a => a[0][0]), arr.map(trapezoidal)]
-  )
 )
 
 integral$.subscribe(
